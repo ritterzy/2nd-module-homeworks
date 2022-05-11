ca2b97169b1f1edccc1621ed70ff78e832ca0088
Abduvali Valiyev <100288755+ritterzy@users.noreply.github.com>
2022-05-11 19:01:03 +0500

    2nd module

---
+++ Homeworks/1 Hw.py
@@ -0,0 +1,74 @@
+
+#! 1 Example
+# class VideoCards:
+#     def __init__(self,card,price):
+#         self.name=card
+#         self.price=price
+#     def PrintPricelist(self):
+#             return f'Type of videocard : {self.name} - {self.price}$'
+# Videocard1=VideoCards("GTX1050",200)
+# Videocard2=VideoCards("GTX1650",240)
+# Videocard3=VideoCards("GTX1660 SuperGaming",480)
+# Videocard4=VideoCards("RTX2060",480)
+# print(Videocard1.PrintPricelist())
+# print(Videocard2.PrintPricelist())
+# print(Videocard3.PrintPricelist())
+# print(Videocard4.PrintPricelist())
+
+
+
+#! 2 Example
+
+# class Bariga:
+#     def __init__(self,amount):
+#         self.dollar=amount
+#         self.euro=amount
+#     def barijim_dollar(self):
+#         return f'Your overall amount in Dollar will be- ^^ {self.dollar / 11145} ^^ $'
+#     def barijim_euro(self):
+#         return f'Your overall amount in Euro will be- ^^ {self.euro / 11765} ^^ euro'
+# a =int(input("Enter your amount in UZD ; "))
+# b = Bariga(a)
+# print(b.barijim_dollar())
+# print(b.barijim_euro())
+
+# ! 3 Example
+# class BarigaDollarov:
+#     def __init__(self,amount):
+#         self.dollar=amount
+#     def barijim_dollar(self):
+#         return f'Your overall amount in UzD will be- ^^ {self.dollar * 11145} ^^ sums'
+# a = int(input("Eneter your amount in dollar ; "))
+# b = BarigaDollarov(a)
+# print(b.barijim_dollar())
+
+# ! 4 Example
+# class Pricelist:
+#     def __init__(self,product):
+#         self.thing=product
+#     def PrintProductslist(self):
+#         return f"{self.thing}"
+# products=Pricelist('''
+# 1 Snickers; 7000 
+# 2 IceTea; 5000 
+# 3 Kurt; 5000 
+# 4 Lays; 4000 15
+# 5 Bruni; 4000 
+# 6 RokiKroki; 6000 
+# 7 Nesquik; 5000 
+# 8 ChocoPie; 4000 
+# 9 Barny; 5000 
+# 10 Nuts; 6000 
+# 11 Arahis; 5000 
+# 12 Rondo"Pechenye"; 4000 
+# 13 KitKat; 6000 
+# 14 Chocolate; 9000 
+# 15 Cookie; 5000 
+# 16 Pepsi; 7000 
+# 17 Adrenaline; 10000 
+# 18 18+; 8000 
+
+# ''')
+# print(products.PrintProductslist())
+
+# ! 5 Example
---
+++ class work/1.py
@@ -0,0 +1,36 @@
+from bisect import bisect_right
+
+
+class Person:
+    species="Homo Sapiens"
+    def __init__(self, name , birth_year):
+        self.name = name
+        self.birth_year=birth_year
+    def Sayhello(self):
+        from datetime import datetime
+        return f'Hi! I am {self.name} f,I am {datetime.today().year - self.birth_year} years old'
+    
+    
+    
+
+
+person1=Person('Iskander',2008)
+person2=Person('Shavkat',2008)
+# person1.name='Iskander'
+# person2.name='Shavkat'
+# print(person1.species,person1.name,person1.birth_year)
+# print(person2.species,person2.name,person2.birth_year)
+print(person1.Sayhello())
+
+
+# class Circle:
+#     def __init__(self,side):
+#         self.side = side
+#     def formula(self):
+#         return (self.side **2)/(4 *3.14)
+#     # def perimetr(self):
+#     #     return self.side * 4
+
+# sq1=Circle(100)
+# print(sq1.formula())
+
